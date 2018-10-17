ec91d4f95789d27a5c91ec0e0b8d8742bcff1855
Manuuzzz <emmanuel.lamon@gmail.com>
2018-10-17 15:10:13 +0200

    update files as wordpress template

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Emmanuel Lamon">
+
+    <title><?php bloginfo('name'); ?></title>
+
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+   
+    <?php wp_head(); ?>
+
+
+</head>
+
