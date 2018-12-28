4e985d6723181fe93b1fedf71a0d0a461bf94cff
Manuuzzz <emmanuel.lamon@gmail.com>
2018-12-28 17:41:30 +0100

    add img

--- header.php
+++ header.php
@@ -13,9 +13,92 @@
     <title><?php bloginfo('name'); ?></title>
 
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+    <!--[if IE 7]>
+	<link rel="stylesheet" href="/css/font-awesome-ie7.min.css">
+	<![endif]-->
+	
+	<script type="application/ld+json">
+	{
+	"@context" : "http://schema.org",
+	"@type" : "Supportersclub",
+	"url" : "https://www.64lesgeiantsdath.be",
+	"logo" : "https://www.64lesgeiantsdath.be/wp-content/uploads/2018/12/logo.png",
+	"image" : "https://www.64lesgeiantsdath.be/wp-content/uploads/2018/12/logo.png",
+	"name" : "64 Les Géiants d'Ath - Supportersclub RSCA",
+	"description" : "We post news and photos from our supportersclub - 64 Les Géiants d'Ath",
+	"telephone" : "",
+	"email" : "emmanuel.lamon@gmail.com",
+	"address" : [{
+	"@type" : "PostalAddress",
+	"streetAddress" : "",
+	"postalCode" : "",
+	"addressLocality" : "",
+	"addressCountry" : "BE"
+	}],
+	"sameAs" : [
+	"https://www.64lesgeiantsdath.be"
+	]
+	}
+	</script>
    
     <?php wp_head(); ?>
 
 
 </head>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Dreams &amp; Creations - Homepage</title>
+	<meta name="keywords" content="Websites, wireframes, ontwerp, design, ontwikkeling, development, webdevelopment, webdesign"/>
+	<meta name="description" content="We helpen starters uit de startblokken te schieten. We nemen hun ideeën kritisch onder de loep, werken wireframes uit, bouwen prototypes en het eindproduct."/>
+	
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+
+	<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+
+	<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+
+	<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+
+	<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+
+	<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+
+	<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+
+	<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+
+	<link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192">
+
+	<link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
+
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+
+	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+
+	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+
+	<meta name="msapplication-TileColor" content="#123646">
+	<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+	
+	
+	
+	<link href='http://fonts.googleapis.com/css?family=Raleway:300,600' rel='stylesheet' type='text/css'>
+
+	
+	<link rel="stylesheet" href="/css/style.css">
+
+	<link rel="stylesheet" href="/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="/css/screen.min.css">
+
+	<link rel="stylesheet" href="/css/screen-mobile.css">
+
+	
+	
 
