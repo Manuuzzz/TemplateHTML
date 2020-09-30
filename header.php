b47c70328722bbe1143ee53e713136b92ce82a3b
manu.lamon@hotmail.com <manu.lamon@hotmail.com>
2020-09-30 10:57:48 +0200

    changes

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 
 <html <?php language_attributes(); ?>>
 
 <head>
 
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="keywords" content="">
     <meta name="author" content="Emmanuel Lamon">
